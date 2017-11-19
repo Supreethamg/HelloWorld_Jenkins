abae94d271f30a140bc91dad365a2f2d1186d04d
Supreethamg <supreethamg@gmail.com>
2017-11-18 17:27:06 -0800

    changed comment text
    changed comment text in App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 /**
  *Hello world  sample 
  Testing github integration and auto detecting git commit.
-TO check if git detects push and triggers build in jenkins123.
+ TO check if git detects push and triggers build in jenkins successfully.
  *
  */
 public class App 
