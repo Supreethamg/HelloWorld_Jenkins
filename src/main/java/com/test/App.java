d4da805685afb9759cecc038d5e2f3b8d96925f9
Supreethamg <supreethamg@gmail.com>
2017-11-18 17:32:59 -0800

    Changed made in comments of App.java
    changes made in App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -3,9 +3,11 @@ package com.test;
 import java.util.UUID;
 
 /**
- *Hello world  sample 
+ *
+Hello world  sample 
  Testing github integration and auto detecting git commit.
  TO check if git detects push and triggers build in jenkins successfully.
+The build was successfull..
  *
  */
 public class App 
